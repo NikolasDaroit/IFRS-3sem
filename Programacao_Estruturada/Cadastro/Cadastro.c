1bf33bedacb483a04bd2ab7faf3972327c7a56a4
NikolasDaroit <nikolastnd@gmail.com>
2018-02-22 10:48:28 -0300

    alteracao no nome de pastas e correcao no cadastro de usuario

--- Programacao_Estruturada/Cadastro/Cadastro.c
+++ Programacao_Estruturada/Cadastro/Cadastro.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define n 5
+#define n 2
 
 struct cliente{
     char nome[20];
@@ -11,19 +11,20 @@ struct cliente{
     char doencas[20];
     int nascimento;
     int idade;
+    int id;
 
 };
 int mostrar_cliente_por_nome(char nome[]);
-int criar_cliente(int posicao);
+int criar_cliente();
 void mostrar_clientes();
 void excluir();
-int i = 0;
 struct cliente clientes[n];
 
 
 int main(int argc, char const *argv[]){
     int posicao = 0;
     int k = 0;
+    int result;
 
     while (k!=6){
         printf("\n1 criar clientes\n2 mostrar todos\n4 pesquisa por nome\n5 excluir por nome\n6 sair\n" );
@@ -32,36 +33,51 @@ int main(int argc, char const *argv[]){
         printf("\n");
         switch ( k ){
             case 1 :
-                posicao = criar_cliente(posicao);
-                
+                result = criar_cliente();
+                if (result == 1){
+                    printf("\nCadastro realizado com sucesso\n");
+
+                }else{
+                    printf("\nNão foi possível realizar este cadastro. Consulte o administrador do sistema.\n");
+                }
+
                 break;
             case 2 :
                 mostrar_clientes();
-            
+
                 break;
             case 4 :
-                mostrar_cliente_por_nome("fulano");
-                
+                mostrar_cliente_por_nome("fulano-1");
+
                 break;
-            case 5 :    
-                excluir(mostrar_cliente_por_nome("fulano"));
-     
+            case 5 :
+                excluir(mostrar_cliente_por_nome("fulano-1"));
+
         }
     }
 }
-int criar_cliente(int posicao){
-    strcpy(clientes[posicao].nome, "fulano");
-    strcpy(clientes[posicao].sexo, "fem");
-    strcpy(clientes[posicao].doencas, "asma");
-    clientes[posicao].nascimento = 1011997;
-    clientes[posicao].idade = 21;
-    
-    return posicao+1;
+int criar_cliente(){
+    int aux ;
+    for (aux=0;aux<n;aux++){
+        if (clientes[aux].id == 0){
+            char buf[20];
+            sprintf(buf, "fulano-%d", aux);
+            strcpy(clientes[aux].nome, buf);
+            strcpy(clientes[aux].sexo, "fem");
+            strcpy(clientes[aux].doencas, "asma");
+            clientes[aux].nascimento = 1011997;
+            clientes[aux].idade = 21;
+            clientes[aux].id = aux+1;
+            return 1;
+        }
+    }
+    return 0;
 
-}
+    }
 void mostrar_clientes(){
     int encontrado = 0;
-    for (int posicao=0;posicao<n;posicao++){
+    int posicao;
+    for (posicao=0;posicao<n;posicao++){
         if (clientes[posicao].nascimento != 0 && clientes[posicao].idade != 0){
             printf("\nNome: %s\nSexo: %s\nDoencas: %s\nNascimento: %i\nIdade: %i\n", clientes[posicao].nome, clientes[posicao].sexo, clientes[posicao].doencas, clientes[posicao].nascimento, clientes[posicao].idade);
             encontrado = 1;
@@ -74,6 +90,7 @@ void mostrar_clientes(){
 
 int mostrar_cliente_por_nome(char nome[]){
     int encontrado = 0;
+    int i;
     for (i=0;i<n;i++){
         if (strcmp(clientes[i].nome, nome) == 0){
             printf("\nCliente encontrado: ");
@@ -85,7 +102,7 @@ int mostrar_cliente_por_nome(char nome[]){
     if (encontrado == 0){
         printf("Cliente nao encontrado\n");
     }
-    return NULL;
+    return 0;
 }
 
 void excluir(int posicao){
@@ -94,4 +111,5 @@ void excluir(int posicao){
     strcpy(clientes[posicao].doencas, "");
     clientes[posicao].nascimento = 0;
     clientes[posicao].idade = 0;
+    clientes[posicao].id = 0;
 }

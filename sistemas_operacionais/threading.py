b118064cc61529569b7f0ecbb8c4884b0e6fa73c
NikolasDaroit <NikolasDaroit@users.noreply.github.com>
2018-06-08 11:40:50 -0300

    Update threading.py
    O problema é que tenho uma classe para o consumidor/produtor que é removida do deque cada vez que a thread é iniciada. Logo, quando não é possível remover certo item do buffer e não tem mais consumidores no deque, ele acaba entrando em looping. As classes de produces e consumer devem ser incorporadas a ConsumerThread e ProducerThread

--- sistemas_operacionais/threading.py
+++ sistemas_operacionais/threading.py
@@ -1,116 +1,229 @@
-import threading
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#import _thread
+#import time
+#import random
+#import sys
+#import threading
+
+from threading import Thread, _allocate_lock, Event
 import time
 import random
-global buffer
-lock = threading._allocate_lock()   #objeto para bloquear/liberar as variáveis
-event = threading.Event()
-class Produtor(threading.Thread):
-    def __init__(self,tipos, id):
-        super(Produtor, self).__init__()
-        self.tipos = list(tipos)
-        self.id = id
-        self.count_item = 0
-        self.repeticoes = 3
-    '''Run() é onde as ações da thread são executadas.'''
+from queue import Queue
+from collections import deque
+queue = Queue(10)
+lock = _allocate_lock()   #objeto para bloquear/liberar as variáveis
+event = Event()
+#length_max = int(sys.argv[1])#
+#s = _thread.allocate_lock()
+
+# buffer de itens, 1 para cada allowed_type, maximo 8 posicoes
+buffer_itens = {1:[], 2:[], 3:[], 4:[], 5:[]}
+global buffer_list
+buffer_list = []
+num_producer = 5#int(input('Digite numero de produceres: '))
+num_producer = (num_producer if num_producer <= 5 else 5)
+num_consumer = 5#int(input('Digite numero de consumeres: '))
+num_consumer = (num_consumer if num_consumer <= 5 else 5)
+print (num_producer, num_consumer)
+
+
+class Producer(object):
+
+    """docstring for Producer"""
+
+    def __init__(self, allowed_types=None):
+        super(Producer, self).__init__()
+        self._id = random.randint(1, 100)
+        self.count = 0
+        if allowed_types is None:
+            self._allowed_types = set()
+        else:
+            self._allowed_types = allowed_types
+
+
+    def get_allowed_type(self):
+        """I'm the 'x' property."""
+
+        return list(self._allowed_types)
+
+    def __str__(self):
+        return str([t for t in self._allowed_types])
+
+    def __repr__(self):
+        return "id: {}".format(self._id)
+class Consumidor(Producer):
+
+    """docstring for Consumidor"""
+
+
+produceres = []
+consumeres = []
+produceres_deque = deque()
+consumeres_deque = deque()
+
+
+def getinputs(prodcons, num):
+    """ prodcons receive \"prod\" to create producer
+   prodcons receive \"cons\" to create consumer 
+   num is how many you want to create
+   """
+
+    allowed_type = -1
+    for i in range(0, num):
+        allowed_types = set()
+
+        """
+        while len(allowed_types) != 5:
+            #allowed_type =  random.randint(0, 5)
+            allowed_type = \
+                int(input('Digite o allowed_type (1 a 5) para dicionar ao %s %i \n0 para finalizar: '
+                       % (prodcons.upper(), i)))
+            if allowed_type == 0:
+                break
+            elif allowed_type > 0 and allowed_type <= 5:
+                allowed_types.add(allowed_type)
+            else:
+                print('Tipo errado, sabe ler n\xc3\xa3o?')
+        """ 
+        if prodcons == 'prod':
+            
+            allowed_types = set()
+            for i in range(1,6):
+                allowed_types.add(random.randint(1, 5))
+            
+            # allowed_types = [1,2,3,4,5]
+            a = Producer(allowed_types)
+            produceres.append(a)
+            produceres_deque.append(a)
+            print("PROD", a._id, a)
+        elif prodcons == 'cons':
+            allowed_types = set()
+            for i in range(1,6):
+                allowed_types.add(random.randint(1, 5))
+            
+            # allowed_types = [1,2,3,4,5]
+            a = Consumidor(allowed_types)
+            consumeres.append(a)
+            consumeres_deque.append(a)
+            print("CONS", a._id, a)
+        
+
+
+getinputs('prod', num_producer)
+getinputs('cons', num_consumer)
+
+
+class Item(object):
+
+    """docstring for Producer"""
+
+    def __init__(
+        self,
+        allowed_type,
+        producer,
+        count,
+        ):
+        super(Item, self).__init__()
+        self.allowed_type = allowed_type
+        self.producer = producer
+        self.count = count
+
+    def __str__(self):
+        return int(self.allowed_type)
+
+    def __repr__(self):
+        return str(self.allowed_type)
+
+"""
+- O producer, a cada produção, cria um item de um allowed_type escolhido aleatoriamente entre os allowed_types que ele está
+apto a produzir;
+- Cada item produzido conterá o allowed_type do item, o ID do seu respectivo producer, qual o número (count) do
+item produzido por aquele producer, e um valor aleatório entre 0 e 3000 (milissegundos);
+- Após produzir um item, o producer deverá dormir um tempo aleatório entre 0 e 3000 milissegundos;
+- Os produceres sempre escrevem na tela os itens que estão producing e o tempo que irão dormir.
+- Os consumeres consomem um item dentre os allowed_types que ele está apto a consumir, mostrando seu
+conteúdo na tela, e depois indo dormir o tempo especificado no item;
+"""
+class ProducerThread(Thread):
+    def __init__(self, types):
+        Thread.__init__(self)
+        self.producer = produceres_deque.popleft()
+        produceres_deque.append(self.producer)
+        self.types = list(types)
     def run(self):
-        while self.tipos:
-            #print("Iniciando produtor ",self.id)
-            tipo = self.tipos.pop(self.tipos.index(random.choice(self.tipos))) #selecionando um tipo aleatóriamente
-            valor = random.randint(0, 3000)
+        while self.types:
+            allowed_type = self.types.pop(random.randrange(len(self.types)))
             lock.acquire()
-            print("Produtor {}, produzindo item do tipo {} ({}ms).".format(self.id, tipo, valor))
-            self.count_item += 1
-            item = Item(tipo, self.id, self.count_item, valor)
-            buffer.append(item)
-            tempo_espera = random.randint(0, 3000)
-            print('Produtor {} irá esperar por {}ms.'.format(self.id, tempo_espera))
-            #print("Finalizando produção ",self.id)
+            print("producer %i runnning"%(self.producer._id))
+            self.producer.count += 1
+            print("producer %i creating item %i"%(self.producer._id,allowed_type))
+            soneca = random.randint(10000, 15000)
+            count = self.producer.count
+            item = Item(allowed_type, self.producer._id, count)
+            buffer_list.append(item)
+            print("producer %i sleeping por %ims"%(self.producer._id,soneca))
+            print("buffer is now %s "%(buffer_list))
             lock.release()
             event.set()
-            time.sleep(float(tempo_espera)/1000)
+            time.sleep(soneca)
         event.clear()
-class Consumidor(threading.Thread):
-    def __init__(self, tipos, id):
-        super(Consumidor, self).__init__()
-        self.tipos = tipos
-        self.id = id
+                
+            
+
+class ConsumerThread(Thread):
+    def __init__(self, types):
+        Thread.__init__(self)
+        self.consumer = consumeres_deque.popleft()
+        consumeres_deque.append(self.consumer)
+        self.types = list(types)
     def run(self):
-        while buffer:
-            #print("Iniciando consumidor", self.id)
-            tipo = random.choice(self.tipos)  # selecionando um tipo aleatóriamente
-            remocao = False
+        while buffer_list:
+            buff = [i.allowed_type for i in buffer_list]
+            allowed_type = random.choice(self.types)
+            print("AT %s - BUFF %s "%(allowed_type, buff))
+            
             lock.acquire()
-            for item in buffer:
-                if item.tipo == tipo:
-                    remocao = True
-                    item_removido = buffer.pop(buffer.index(item))
-                    print('Consumidor {} consumiu o item {}(tipo {}) do produtor {} ({}ms)'
-                          .format(self.id, item_removido.count_produtor, item_removido.tipo,
-                                  item_removido.id_produtor,
-                                  item_removido.valor))
-                    print("Consumidor {} irá esperar por {}ms".format(self.id, item_removido.valor))
-                    tempo_espera = float(item_removido.valor) / 1000
-                    #print("finalizando consumidor ", self.id)
+            print("consumer %i runnning. Allowed %s Buffer %s"%(self.consumer._id, allowed_type, buffer_list))
+            removed = False
+            for item in buffer_list:
+
+                if allowed_type == item.allowed_type:
+                    removed = True
+                    buffer_list.remove(item)
+                    print("consumer %i consuming item %i"%(self.consumer._id,allowed_type))
+                    self.consumer.count += 1
+                    print("consumindo item %i"%allowed_type)
+                    soneca = random.randint(10000, 15000)
+                    count = self.consumer.count                 
+                    print("consumer %i sleeping por %ims"%(self.consumer._id,soneca))
+                    print("buffer is now %s "%(buffer_list))
                     lock.release()
-                    time.sleep(tempo_espera)
-            if remocao is False:
+                    time.sleep(soneca)
+            if removed is False:
                 lock.release()
-class Item:
-    def __init__(self, tipo, id_produtor, count_produtor, valor):
-        self.tipo = tipo
-        self.id_produtor = id_produtor
-        self.count_produtor = count_produtor
-        self.valor = valor
-    @property
-    def tipo(self):
-        return self._tipo
-    @property
-    def id_produtor(self):
-        return self._id_produtor
-    @property
-    def count_produtor(self):
-        return self._count_produtor
-    @property
-    def valor(self):
-        return self._valor
-    @tipo.setter
-    def tipo(self, value):
-        self._tipo = value
-    @id_produtor.setter
-    def id_produtor(self, value):
-        self._id_produtor = value
-    @count_produtor.setter
-    def count_produtor(self, value):
-        self._count_produtor = value
-    @valor.setter
-    def valor(self, value):
-        self._valor = value
-if __name__ == '__main__':
-    buffer = []
-    lista_threads = []
-    '''while True:
-        try:
-            a, b = map(int, input("Digite a quantidade de produtores e consumidores, separados por virgula, a serem criados,"
-                        " respectivamente(No max 5 no total): ").split(','))
-            break
-        except ValueError:
-            print("Digite novamente")'''
-    a,b = 3,2
-    for i in range(a):
-        '''tipo_produtor = tuple(input("Digite os tipos de itens que o Produtor {}"
-                                    " irá produzir: ".format(i)).split(','))   #tuple com os tipos do produtor.'''
-        tipo_produtor = ('A', "B")
-        t = Produtor(tipo_produtor, i)
-        t.start()  #start() inicia o método run do objeto thread
-        lista_threads.append(t)
-    for i in range(b):
-        '''tipo_consumidor = tuple(input("Digite os tipos de itens que o Consumidor {}"
-                                      " irá consumir: ".format(i)).split(','))  # tuple com os tipos do consumidor.'''
-        tipo_consumidor = ('A', 'B')
-        t = Consumidor(tipo_consumidor, i)
-        t.start()
-        lista_threads.append(t)
-    '''join() avisa a thread main para esperar essa thread específica finalizar antes de executar a próxima instrução'''
-    for thread in lista_threads:
+
+                    
+                   
+def createthreads():
+    threads = []
+    for producer in produceres:
+        thread = ProducerThread(producer.get_allowed_type())
+        threads.append(thread)
+        thread.start()
+
+    for consumer in consumeres:
+        thread = ConsumerThread(consumer.get_allowed_type())
+        threads.append(thread)
+        thread.start()
+    
+    random.shuffle(threads)
+    for thread in threads:
         thread.join()
-    print("Execução finalizada.")
+
+    
+
+while True:
+    
+    createthreads()
